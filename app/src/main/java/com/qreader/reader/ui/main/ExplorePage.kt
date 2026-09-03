9d1f2179ba00d35848c84987f75a874609988663
hzp <yearhe@outlook.com>
2026-09-03 13:14:10 +0800

    修复 drawRect import 错误
    - drawRect 是 DrawScope 成员函数，不需要显式 import

--- app/src/main/java/com/qreader/reader/ui/main/ExplorePage.kt
+++ app/src/main/java/com/qreader/reader/ui/main/ExplorePage.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.drawscope.drawRect
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.TextStyle
